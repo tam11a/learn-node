06d746fecb14d4185433db0f88f77749c7150b9e
tam11a <ibrahimsadiktamim@gmail.com>
2023-04-15 03:54:52 +0600

    init repo

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Initialize Database
+require("./config/db");
+
+app.use(express.json());
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
